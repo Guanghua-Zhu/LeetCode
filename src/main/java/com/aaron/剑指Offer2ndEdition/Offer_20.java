47618568f609b17abc6af88f518125bfdf35a847
Aaron Zhu <zghcivilization@163.com>
2022-02-11 23:29:45 +0800

    1. [Add]: 剑指 Offer·第2版 20, 表示数值的字符串: DFA 确定有限状态自动机

--- src/main/java/com/aaron/剑指Offer2ndEdition/Offer_20.java
+++ src/main/java/com/aaron/剑指Offer2ndEdition/Offer_20.java
@@ -84,6 +84,9 @@ package com.aaron.剑指Offer2ndEdition;
 // 👍 285 👎 0
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 剑指 Offer 20, 表示数值的字符串
  * @author Aaron Zhu
@@ -98,143 +101,322 @@ public class Offer_20 {
      * 硬写……
      */
     public static class Solution {
-    public boolean isNumber(String s) {
-        if( s==null || s.length()==0 ) {
-            return false;
+        public boolean isNumber(String s) {
+            if( s==null || s.length()==0 ) {
+                return false;
+            }
+            // 移除前后空格
+            s = removeBlank(s);
+            if( s==null ) {
+                return false;
+            }
+            int eCount = 0;
+            for(char ch :s.toCharArray() ) {
+                if( ch=='e' || ch=='E' ) {
+                    eCount++;
+                    if( eCount>1 ) {
+                        return false;
+                    }
+                    continue;
+                }
+                if( ch=='+' || ch=='-' || ch=='.' ) {
+                    continue;
+                }
+                if( ch>='0' && ch<='9' ) {
+                    continue;
+                }
+                return false;
+            }
+            String[] array = s.split("[eE]", -1);
+            // 检查第一部分
+            boolean isValid = isInteger(array[0]) || isFloat(array[0]);
+            if( !isValid ) {
+                return false;
+            }
+            // 检查第二部分
+            if( eCount!=0 ) {
+                return isInteger(array[1]);
+            }
+            return true;
         }
-        // 移除前后空格
-        s = removeBlank(s);
-        if( s==null ) {
-            return false;
+
+        private String removeBlank(String s) {
+            char[] chars = s.toCharArray();
+            int startIndex = 0;
+            while ( startIndex < chars.length ) {
+                if( chars[startIndex] != ' ' ) {
+                    break;
+                }
+                startIndex++;
+            }
+
+            int endIndex = chars.length-1;
+            while ( endIndex >=0 ) {
+                if( chars[endIndex] != ' ' ) {
+                    break;
+                }
+                endIndex--;
+            }
+
+            if( startIndex==chars.length || endIndex==-1 ) {
+                return null;
+            }
+
+            return s.substring(startIndex, endIndex+1);
         }
-        int eCount = 0;
-        for(char ch :s.toCharArray() ) {
-            if( ch=='e' || ch=='E' ) {
-                eCount++;
-                if( eCount>1 ) {
+
+        /**
+         * 是否为整数
+         * @param num
+         * @return
+         */
+        private boolean isInteger(String num) {
+            if( num==null || num.length()==0 ) {
+                return false;
+            }
+            char[] chars = num.toCharArray();
+            // 存在符号位, 至少需要一位数字
+            if( chars.length==1  && (chars[0]=='+' || chars[0] =='-') ) {
+                return false;
+            }
+
+            for(int i=0; i<chars.length; i++) {
+                char ch = chars[i];
+                if ( i==0 && (ch=='+' || ch=='-') ) {
+                    continue;
+                }
+                if( ch>='0' && ch<='9' ) {
+                    continue;
+                }
+                return false;
+            }
+            return true;
+        }
+
+        /**
+         * 是否为小数
+         * @param num
+         * @return
+         */
+        private boolean isFloat(String num) {
+            if( num==null || num.length()==0 ) {
+                return false;
+            }
+
+            char firstCh = num.charAt(0);
+            // 存在符号位
+            if( firstCh=='+' || firstCh=='-' ) {
+                if( num.length() == 1 ) {
+                    // 至少需要一位数字
+                    return false;
+                } else {
+                    // 移除符号位
+                    num = num.substring(1);
+                }
+            }
+
+            int dotCount = 0;
+            int digitCount = 0;
+            for (char ch: num.toCharArray()) {
+                // 统计小数点次数
+                if( ch == '.' ) {
+                    dotCount++;
+                    continue;
+                }
+                if( ch>='0' && ch<='9' ) {
+                    digitCount++;
+                    continue;
+                }
+                return false;
+            }
+
+            if( dotCount!=1 || digitCount==0 ) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+
+    /**
+     * DFA 确定有限状态自动机
+     */
+    public static class Solution2 {
+        /**
+         * 状态转移规则, 当前状态 + 输入类型 -> 下一个状态
+         * 第一层Map, key: 当前状态
+         * 第二层Map, key: 输入类型; value: 下一个状态
+         */
+        private static Map<State, Map<InputType, State>> transferRule;
+
+        /**
+         * 定义状态转移规则
+         */
+        static{
+            transferRule = new HashMap<>();
+
+            Map<InputType, State> map = new HashMap<>();
+            map.put( InputType.BLANK, State.START_BANK );
+            map.put( InputType.SIGN, State.NUMBER_SIGN );
+            map.put( InputType.NUMBER, State.INTEGER );
+            map.put( InputType.DOT, State.DOT_WITHOUT_INTEGER );
+            transferRule.put( State.START_BANK, map );
+
+            map = new HashMap<>();
+            map.put( InputType.NUMBER, State.INTEGER );
+            map.put( InputType.DOT, State.DOT_WITHOUT_INTEGER );
+            transferRule.put( State.NUMBER_SIGN, map );
+
+            map = new HashMap<>();
+            map.put( InputType.NUMBER, State.INTEGER );
+            map.put( InputType.DOT, State.DOT_WITH_INTEGER );
+            map.put( InputType.POWER, State.POWER_LOGO );
+            map.put( InputType.BLANK, State.END_BLANK );
+            transferRule.put( State.INTEGER, map );
+
+            map = new HashMap<>();
+            map.put( InputType.BLANK, State.END_BLANK );
+            map.put( InputType.POWER, State.POWER_LOGO );
+            map.put( InputType.NUMBER, State.FLOAT );
+            transferRule.put( State.DOT_WITH_INTEGER, map );
+
+            map = new HashMap<>();
+            map.put( InputType.NUMBER, State.FLOAT );
+            transferRule.put( State.DOT_WITHOUT_INTEGER, map );
+
+            map = new HashMap<>();
+            map.put( InputType.NUMBER, State.FLOAT );
+            map.put( InputType.POWER, State.POWER_LOGO );
+            map.put( InputType.BLANK, State.END_BLANK );
+            transferRule.put( State.FLOAT, map );
+
+            map = new HashMap<>();
+            map.put( InputType.SIGN, State.POWER_SIGN );
+            map.put( InputType.NUMBER, State.POWER_NUMBER );
+            transferRule.put( State.POWER_LOGO, map );
+
+            map = new HashMap<>();
+            map.put( InputType.NUMBER, State.POWER_NUMBER );
+            transferRule.put( State.POWER_SIGN, map );
+
+            map = new HashMap<>();
+            map.put( InputType.BLANK, State.END_BLANK );
+            map.put( InputType.NUMBER, State.POWER_NUMBER );
+            transferRule.put( State.POWER_NUMBER, map );
+
+            map = new HashMap<>();
+            map.put( InputType.BLANK, State.END_BLANK );
+            transferRule.put( State.END_BLANK, map );
+        }
+
+        public boolean isNumber(String s) {
+            if( s==null || s.length()==0 ) {
+                return false;
+            }
+
+            char[] chars = s.toCharArray();
+            // DFA的起始状态为 <起始空格>
+            State state = State.START_BANK;
+            for (char ch : chars) {
+                // 根据字符计算输入类型
+                InputType inputType = InputType.calcInputType(ch);
+                // 根据当前状态获取下一步的状态转移规则
+                Map<InputType, State> map = transferRule.get(state);
+                // 根据当前状态的转移规则、输入类型计算出下一个状态
+                State nextState = map.get(inputType);
+                if( nextState==null ) {
+                    // 下一个状态为空, 说明相应的输入类型不合法
                     return false;
                 }
-                continue;
+                // 更新当前状态
+                state = nextState;
             }
-            if( ch=='+' || ch=='-' || ch=='.' ) {
-                continue;
+
+            // 如果最终的状态为可接受的, 则返回true; 反之为false
+            return state.isAccept();
+        }
+
+        /**
+         * 输入类型
+         */
+        public enum InputType {
+            OTHER(0, "非法输入"),
+            BLANK(1, "空格"),
+            NUMBER(2, "数字"),
+            SIGN(3, "符号位"),
+            DOT(4, "小数点"),
+            POWER(5,"指数符号"),
+            ;
+
+            private int code;
+
+            private String name;
+
+            InputType(int code, String name) {
+                this.code = code;
+                this.name = name;
             }
-            if( ch>='0' && ch<='9' ) {
-                continue;
+
+            /**
+             * 根据字符计算输入类型
+             * @param ch
+             * @return
+             */
+            public static InputType calcInputType(char ch) {
+                if ( ch==' ' ) {
+                    return BLANK;
+                } else if( ch>='0' && ch<='9' ) {
+                    return NUMBER;
+                } else if ( ch=='+' || ch=='-' ) {
+                    return SIGN;
+                } else if ( ch=='.' ) {
+                    return DOT;
+                } else if ( ch=='e' || ch=='E' ) {
+                    return POWER;
+                }
+                return OTHER;
             }
-            return false;
         }
-        String[] array = s.split("[eE]", -1);
-        // 检查第一部分
-        boolean isValid = isInteger(array[0]) || isFloat(array[0]);
-        if( !isValid ) {
-            return false;
+
+        /**
+         * 状态
+         */
+        public enum State {
+            START_BANK("起始空格", false),
+            NUMBER_SIGN("数字的符号位", false),
+            INTEGER("整数部分", true),
+            DOT_WITH_INTEGER("左侧有整数的小数点", true),
+            DOT_WITHOUT_INTEGER("左侧无整数的小数点", false),
+            FLOAT("小数部分", true),
+            POWER_LOGO("指数符号", false),
+            POWER_SIGN("指数的符号位", false),
+            POWER_NUMBER("指数部分的整数", true),
+            END_BLANK( "结束空格", true),
+            ;
+
+            /**
+             * 状态名称
+             */
+            private String name;
+
+            /**
+             * 是否为接受状态
+             * true: 接受状态
+             * false: 非接受状态
+             */
+            private boolean accept;
+
+            State(String name, boolean accept) {
+                this.name = name;
+                this.accept = accept;
+            }
+
+            public boolean isAccept() {
+                return accept;
+            }
         }
-        // 检查第二部分
-        if( eCount!=0 ) {
-            return isInteger(array[1]);
-        }
-        return true;
     }
-
-    private String removeBlank(String s) {
-        char[] chars = s.toCharArray();
-        int startIndex = 0;
-        while ( startIndex < chars.length ) {
-            if( chars[startIndex] != ' ' ) {
-                break;
-            }
-            startIndex++;
-        }
-
-        int endIndex = chars.length-1;
-        while ( endIndex >=0 ) {
-            if( chars[endIndex] != ' ' ) {
-                break;
-            }
-            endIndex--;
-        }
-
-        if( startIndex==chars.length || endIndex==-1 ) {
-            return null;
-        }
-
-        return s.substring(startIndex, endIndex+1);
-    }
-
-    /**
-     * 是否为整数
-     * @param num
-     * @return
-     */
-    private boolean isInteger(String num) {
-        if( num==null || num.length()==0 ) {
-            return false;
-        }
-        char[] chars = num.toCharArray();
-        // 存在符号位, 至少需要一位数字
-        if( chars.length==1  && (chars[0]=='+' || chars[0] =='-') ) {
-            return false;
-        }
-
-        for(int i=0; i<chars.length; i++) {
-            char ch = chars[i];
-            if ( i==0 && (ch=='+' || ch=='-') ) {
-                continue;
-            }
-            if( ch>='0' && ch<='9' ) {
-                continue;
-            }
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * 是否为小数
-     * @param num
-     * @return
-     */
-    private boolean isFloat(String num) {
-        if( num==null || num.length()==0 ) {
-            return false;
-        }
-
-        char firstCh = num.charAt(0);
-        // 存在符号位
-        if( firstCh=='+' || firstCh=='-' ) {
-            if( num.length() == 1 ) {
-                // 至少需要一位数字
-                return false;
-            } else {
-                // 移除符号位
-                num = num.substring(1);
-            }
-        }
-
-        int dotCount = 0;
-        int digitCount = 0;
-        for (char ch: num.toCharArray()) {
-            // 统计小数点次数
-            if( ch == '.' ) {
-                dotCount++;
-                continue;
-            }
-            if( ch>='0' && ch<='9' ) {
-                digitCount++;
-                continue;
-            }
-            return false;
-        }
-
-        if( dotCount!=1 || digitCount==0 ) {
-            return false;
-        }
-
-        return true;
-    }
-
-}
 }
 
 
