c2c4c9703132893e05d54fffa84cca51caf34860
Aaron Zhu <zghcivilization@163.com>
2022-04-27 13:17:35 +0800

    1. [Add]: LeetCode: 72, 编辑距离

---
+++ src/main/java/com/aaron/Leetcode/EditDistance_72.java
@@ -0,0 +1,109 @@
+package com.aaron.LeetCode;
+
+import java.util.*;
+
+//给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数 。 
+//
+// 你可以对一个单词进行如下三种操作： 
+//
+// 
+// 插入一个字符 
+// 删除一个字符 
+// 替换一个字符 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：word1 = "horse", word2 = "ros"
+//输出：3
+//解释：
+//horse -> rorse (将 'h' 替换为 'r')
+//rorse -> rose (删除 'r')
+//rose -> ros (删除 'e')
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：word1 = "intention", word2 = "execution"
+//输出：5
+//解释：
+//intention -> inention (删除 't')
+//inention -> enention (将 'i' 替换为 'e')
+//enention -> exention (将 'n' 替换为 'x')
+//exention -> exection (将 'n' 替换为 'c')
+//exection -> execution (插入 'u')
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= word1.length, word2.length <= 500 
+// word1 和 word2 由小写英文字母组成 
+// 
+// Related Topics 字符串 动态规划 
+// 👍 2327 👎 0
+
+
+/**
+ * 72, 编辑距离
+ * @author Aaron Zhu
+ * @date 2022-4-26
+ */
+public class EditDistance_72{
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    /**
+     * DP动态规划
+     */
+    public static class Solution {
+        public int minDistance(String word1, String word2) {
+            if( word1.length()==0 && word2.length()==0 ) {
+                return 0;
+            } else if ( word1.length()==0 ) {
+                return word2.length();
+            } else if( word2.length()==0 ) {
+                return word1.length();
+            }
+
+            int m = word1.length();
+            int n = word2.length();
+            int[][] dp = new int[m+1][n+1];
+
+            for (int i=0; i<m+1; i++) {
+                dp[i][0] = i;
+            }
+
+            for (int j=0; j<n+1; j++) {
+                dp[0][j] = j;
+            }
+
+            for (int i=1; i<m+1; i++) {
+                for (int j=1; j<n+1; j++) {
+                    if( word1.charAt(i-1) == word2.charAt(j-1) ) {
+                        dp[i][j] = dp[i-1][j-1];
+                    } else {
+                        dp[i][j] = min( dp[i-1][j], dp[i][j-1], dp[i-1][j-1] ) +1;
+                    }
+                }
+            }
+
+            return dp[m][n];
+        }
+
+        private int min(int a, int b, int c) {
+            int temp = Math.min(a,b);
+            int res= Math.min(temp, c);
+            return res;
+        }
+    }
+
+}
