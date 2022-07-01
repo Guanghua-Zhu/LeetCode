f95c270194087e6850678663d8de2b8e30ec9327
Aaron Zhu <zghcivilization@163.com>
2022-07-01 13:23:23 +0800

    1. [Add]: temp file

---
+++ src/main/java/com/temp/leetcode/editor/cn/MaximalSquare_221.java
@@ -0,0 +1,62 @@
+package com.temp.leetcode.editor.cn;
+
+import java.util.*;
+
+//在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"]
+//,["1","0","0","1","0"]]
+//输出：4
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [["0","1"],["1","0"]]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：matrix = [["0"]]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 300 
+// matrix[i][j] 为 '0' 或 '1' 
+// 
+// Related Topics 数组 动态规划 矩阵 👍 1184 👎 0
+
+
+/**
+ * 221, 最大正方形
+ * @author Aaron Zhu
+ * @date 2022-7-1
+ */
+public class MaximalSquare_221{
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
