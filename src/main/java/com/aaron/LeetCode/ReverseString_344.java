6ace2bc274793e7ed22154bb5722a0a8418de21f
Aaron Zhu <zghcivilization@163.com>
2022-12-08 13:29:14 +0800

    1. [Add]: 344, 反转字符串

---
+++ src/main/java/com/aaron/LeetCode/ReverseString_344.java
@@ -0,0 +1,61 @@
+package com.aaron.LeetCode;
+
+import java.util.*;
+
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。 
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = ["h","e","l","l","o"]
+//输出：["o","l","l","e","h"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = ["H","a","n","n","a","h"]
+//输出：["h","a","n","n","a","H"] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁵ 
+// s[i] 都是 ASCII 码表中的可打印字符 
+// 
+//
+// Related Topics 双指针 字符串 👍 698 👎 0
+
+
+/**
+ * 344, 反转字符串
+ * @author Aaron Zhu
+ * @date 2022-12-8
+ */
+public class ReverseString_344{
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    public static class Solution {
+        public void reverseString(char[] s) {
+            int p = 0;
+            int q = s.length-1;
+            while (p<q) {
+                char ch = s[p];
+                s[p] = s[q];
+                s[q] = ch;
+                p++;
+                q--;
+            }
+        }
+    }
+
+}
