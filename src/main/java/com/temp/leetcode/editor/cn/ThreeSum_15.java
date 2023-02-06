8ce0774da32577e42146e8df977f8892f743871c
Aaron Zhu <zghcivilization@163.com>
2023-02-06 13:28:17 +0800

    1. [Add]: temp file

--- src/main/java/com/temp/leetcode/editor/cn/ThreeSum_15.java
+++ src/main/java/com/temp/leetcode/editor/cn/ThreeSum_15.java
@@ -71,23 +71,39 @@ public class ThreeSum_15{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-
+        List<List<Integer>> res = new ArrayList<>();
         int size = nums.length;
+        if( size<3 ) {
+            return res;
+        }
+
+        Arrays.sort(nums);
+        if( nums[0]>0 ) {
+            return res;
+        }
+
 
         for (int i=0; i<size-2; i++) {
+            if( i!=0 && nums[i]==nums[i-1] ) {
+                continue;
+            }
+
             for (int j=i+1; j<size-1; j++) {
+                if( j!=i+1 && nums[j] == nums[j-1] ) {
+                    continue;
+                }
+
                 for (int k=j+1; k<size; k++) {
+                    if( k!=j+1 && nums[k] == nums[k-1] ) {
+                        continue;
+                    }
+
                     if( nums[i]+nums[j]+nums[k]==0 ) {
-                        res.add( Arrays.asList(nums[i], nums[j], nums[k]) );
+                        res.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     }
                  }
             }
         }
-
-        res.stream()
-
-
         return res;
     }
 }
