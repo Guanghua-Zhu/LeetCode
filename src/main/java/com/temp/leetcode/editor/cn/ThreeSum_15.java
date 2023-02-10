1eb3aabb9739932de433d9d131d7c7ca2abf095d
Aaron Zhu <zghcivilization@163.com>
2023-02-10 13:31:49 +0800

    1. [Add]: temp file

--- src/main/java/com/temp/leetcode/editor/cn/ThreeSum_15.java
+++ src/main/java/com/temp/leetcode/editor/cn/ThreeSum_15.java
@@ -1,6 +1,6 @@
 package com.temp.leetcode.editor.cn;
 
-import org.graalvm.compiler.lir.LIR;
+
 
 import java.util.*;
 
@@ -65,6 +65,9 @@ public class ThreeSum_15{
     
     public static void main(String[] args) {
         Solution solution = new Solution();
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+        solution.threeSum( nums );
+        System.out.println("gg");
     }
 }
 
@@ -73,37 +76,38 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         int size = nums.length;
-        if( size<3 ) {
-            return res;
-        }
-
         Arrays.sort(nums);
-        if( nums[0]>0 ) {
-            return res;
-        }
-
 
         for (int i=0; i<size-2; i++) {
-            if( i!=0 && nums[i]==nums[i-1] ) {
+            if( nums[i] > 0 ) {
+                break;
+            }
+            if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            for (int j=i+1; j<size-1; j++) {
-                if( j!=i+1 && nums[j] == nums[j-1] ) {
-                    continue;
+            for (int j = i + 1, k = size - 1; j<k; ) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    res.add( Arrays.asList(nums[i], nums[j], nums[j]) );
+                    while (j<k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j<k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                } else if (sum<0) {
+                    while (j<k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                } else if (sum>0) {
+                    while (j<k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
                 }
-
-                for (int k=j+1; k<size; k++) {
-                    if( k!=j+1 && nums[k] == nums[k-1] ) {
-                        continue;
-                    }
-
-                    if( nums[i]+nums[j]+nums[k]==0 ) {
-                        res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    }
-                 }
             }
         }
+
         return res;
     }
 }
