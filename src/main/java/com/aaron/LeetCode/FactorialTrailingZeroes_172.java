0a754077c9233c040530a029704ea3182c626db1
Aaron Zhu <zghcivilization@163.com>
2023-07-14 13:23:52 +0800

    1. [Add]: 172, 阶乘后的零

---
+++ src/main/java/com/aaron/LeetCode/FactorialTrailingZeroes_172.java
@@ -0,0 +1,83 @@
+package com.aaron.LeetCode;
+
+import java.util.*;
+
+//给定一个整数 n ，返回 n! 结果中尾随零的数量。 
+//
+// 提示 n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 3
+//输出：0
+//解释：3! = 6 ，不含尾随 0
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 5
+//输出：1
+//解释：5! = 120 ，有一个尾随 0
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：n = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 10⁴ 
+// 
+//
+// 
+//
+// 进阶：你可以设计并实现对数时间复杂度的算法来解决此问题吗？ 
+//
+// Related Topics 数学 👍 769 👎 0
+
+
+/**
+ * 172, 阶乘后的零
+ * @author Aaron Zhu
+ * @date 2023-7-14
+ */
+public class FactorialTrailingZeroes_172{
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    public static class Solution {
+        public int trailingZeroes(int n) {
+            int res = 0;
+            for (int i=5; i<=n; i=i+5) {
+                for (int j=i; j%5==0; j=j/5) {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+
+    public static class Solution1 {
+        public int trailingZeroes(int n) {
+            int ans = 0;
+            while (n > 0) {
+                ans += n/5;
+                n /= 5;
+            }
+            return ans;
+        }
+    }
+
+}
